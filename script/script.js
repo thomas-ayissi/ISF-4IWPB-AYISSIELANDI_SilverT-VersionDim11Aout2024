0fb0febc64aa4dfc437a78659f4d1c45c803ff80
Thomas Ayissi <thomas8sea@gmail.com>
2024-08-14 21:06:19 +0200

    12K-P (Départ de Iteration 7 Design Restored - Debut Travail Lun 12 Aout 2024 Soir) 1=>Ajout Regles dans Medias Queries pour : articles (texte sous l'image), logo 2=>Trop de problèmes, d'incompatibilités. Nous supprimons des règles dans les Medias Queries et restons sur le plus simple.
    3=> Leçon : toujours faire els Medias-Queries AU DEBUT
    4=>

--- script/script.js
+++ script/script.js
@@ -1,3 +1,9 @@
-document.getElementById('hamburger').addEventListener('click', function() {
-    document.querySelector('.top-menu').classList.toggle('active');
-});
+function showMenu() {
+    var menu = document.getElementById('dropdown-menu');
+    menu.style.display = 'flex';
+}
+
+function hideMenu() {
+    var menu = document.getElementById('dropdown-menu');
+    menu.style.display = 'none';
+}
