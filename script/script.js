72cfdee31fd8ffb36f6ed5fd1a7578448664ea58
Thomas Ayissi <thomas8sea@gmail.com>
2024-08-16 18:41:42 +0200

    12K-Q-7 Quater Pré BAT (Départ de Iteration 7 Design Restored - Travail Ven 16 Aout 2024 Après Midi 16h49) 1=> Polices définies directement grâce à un lien sur le fichier ttf 2=>Fichiers et dossiers non requis par le prof supprimés.

--- script/script.js
+++
@@ -1,9 +0,0 @@
-function showMenu() {
-    var menu = document.getElementById('dropdown-menu');
-    menu.style.display = 'flex';
-}
-
-function hideMenu() {
-    var menu = document.getElementById('dropdown-menu');
-    menu.style.display = 'none';
-}
